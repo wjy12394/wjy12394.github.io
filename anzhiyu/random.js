94d56ddc917bc4a7bced621f637cf60f595e4453
wjy12394 <wangjingyu20010419@163.com>
2024-10-31 18:58:45 +0800

    Site updated: 2024-10-31 18:58:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/29/ADSIPC/","2024/10/29/BeckhoffSimulink/","2024/10/24/hello-world/"];function toRandomPost(){
+var posts=["2024/10/29/ADSIPC/","2024/10/24/hello-world/","2024/10/29/BeckhoffSimulink/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
